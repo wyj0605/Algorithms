7212053b3cb192c67a41d48908483959bcabba25
wangyijun <wyj0605@gmail.com>
2018-10-17 09:57:11 +0800

    冒泡排序

---
+++ Bubble_Sort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def Bubble_Sort(alist):
+    n=len(alist)
+    for i in range(n):
+        for j in range(n-1-i):
+            if alist[j] > alist[j+1]:
+                alist[j], alist[j+1] = alist[j+1], alist[j]
+    return alist
+
+
+alist=[2,5,1,6,34534,345345,345,345,34,63,456]
+
+Bubble_Sort(alist)
+print (alist)
